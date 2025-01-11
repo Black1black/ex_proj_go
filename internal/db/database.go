3fcaa7bf509246aec4a8acccd3ed55cc85a24350
MSI <msi@gmail.com>
2025-01-11 16:43:50 +0500

    Поменял логику в database.go

--- internal/db/database.go
+++ internal/db/database.go
@@ -3,41 +3,63 @@ package db
 import (
 	"ex_proj_go/internal/models"
 	"fmt"
-	"log"
-
 	"os"
 
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
+// type DatabaseConfig struct {
+//     Host     string `yaml:"host"`
+//     Port     int `yaml:"port"`
+//     User     string `yaml:"user"`
+//     Password string `yaml:"password"`
+//     DBName   string `yaml:"dbname"`
+// }
 
-func InitDB() {
-	configFile, err := os.Open("configs/config.yaml")
+// type Config struct {
+//     Database DatabaseConfig `yaml:"database"`
+// }
+
+type Config struct {
+	Database struct {
+		Host     string `yaml:"host"`
+		Port     int    `yaml:"port"`
+		User     string `yaml:"user"`
+		Password string `yaml:"password"`
+		DBName   string `yaml:"dbname"`
+	} `yaml:"database"`
+}
+
+func InitDB() (*gorm.DB, error) {
+	data, err := os.ReadFile("configs/config.yaml")
 	if err != nil {
-		log.Fatalf("Failed to load config file: %v", err)
-	}
-	defer configFile.Close()
-
-	var config map[string]map[string]string
-	if err := yaml.NewDecoder(configFile).Decode(&config); err != nil {
-		log.Fatalf("Failed to parse config file: %v", err)
+		return nil, fmt.Errorf("failed to read config file: %w", err)
 	}
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		config["database"]["host"], config["database"]["user"], config["database"]["password"],
-		config["database"]["dbname"], config["database"]["port"])
-
-	var dbErr error
-	DB, dbErr = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if dbErr != nil {
-		log.Fatalf("Failed to connect to database: %v", dbErr)
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, fmt.Errorf("failed to parse config file: %w", err)
 	}
 
-	// Auto-migrate models
-	if err := DB.AutoMigrate(&models.User{}); err != nil {
-		log.Fatalf("Failed to migrate database: %v", err)
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
+		cfg.Database.Host,
+		cfg.Database.User,
+		cfg.Database.Password,
+		cfg.Database.DBName,
+		cfg.Database.Port,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
 	}
+	// TODO - Вывести миграции в отдельный файл/функцию и сделать миграцию по всем моделям
+	if err := db.AutoMigrate(&models.User{}); err != nil {
+		return nil, fmt.Errorf("failed to migrate database: %w", err)
+	}
+
+	return db, nil
 }
